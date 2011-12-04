ece779833f10f22ee18e1a73f51cc6e711630b7d
Alex Baranosky <alexander.baranosky@gmail.com>
2011-12-04 00:19:31 -0500

    moved files

--- src/retreat1/core.clj
+++ src/retreat1/core.clj
@@ -1,1 +1,30 @@
-(ns retreat1.core)
+(ns retreat1.core
+  (:use [retreat1.core])
+  (:use [clojure.set])
+  (:use [midje.sweet]))
+
+(defn neighbors [& people]
+  (difference
+    (set
+      (for [{:keys [x y]} people
+            i (range (dec x) (+ 2 x))
+            j (range (dec y) (+ 2 y))]
+        {:x i :y j}))
+    people))
+
+(defn survivors [people]
+  (set (for [p people
+             :when (#{2 3} (count (neighbors p)))]
+         p)))
+
+(defn newborns [people]
+  (let [unborn (apply neighbors people)]
+    (set
+      (for [ub unborn
+            :when (= 3 (count (neighbors ub)))]
+        ub))))
+
+(defn next-world [people]
+  (union (survivors people)
+    (newborns people)))
+ 
--- test/retreat1/test/core.clj
+++ test/retreat1/test/core.clj
@@ -3,31 +3,6 @@
   (:use [clojure.set])
   (:use [midje.sweet]))
 
-(defn neighbors [& people]
-  (difference
-    (set
-      (for [{:keys [x y]} people
-            i (range (dec x) (+ 2 x))
-            j (range (dec y) (+ 2 y))]
-        {:x i :y j}))
-    people))
-
-(defn survivors [people]
-  (set (for [p people
-             :when (#{2 3} (count (neighbors p)))]
-         p)))
-
-(defn newborns [people]
-  (let [unborn (apply neighbors people)]
-    (set
-      (for [ub unborn
-            :when (= 3 (count (neighbors ub)))]
-        ub))))
-
-(defn next-world [people]
-  (union (survivors people) 
-         (newborns people)))
-
 (doseq [?items (map #(repeat % :x) [2 3])]
   (fact "stupid test of internal function"
     (survivors #{  {:x 1 :y 2} }) => #{{:x 1 :y 2}}
